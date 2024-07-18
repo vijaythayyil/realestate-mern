c22bdfb27f736f1a20f384a529ac19dd35b50f53
Vijay Thayyil <68293066+vijaythayyil@users.noreply.github.com>
2024-07-18 19:14:34 -0400

    Database connected, bandkend created, express setup

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

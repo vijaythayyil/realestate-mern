8907a40e80cfa57e16534f8fef42cd3c391147b5
Vijay Thayyil <68293066+vijaythayyil@users.noreply.github.com>
2024-07-18 17:35:01 -0400

    Routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import SignUp from "../src/pages/SignUp";
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

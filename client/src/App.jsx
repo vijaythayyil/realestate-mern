a3735ccc6558e2be3b7e1595259d559ab804b961
Vijay Thayyil <68293066+vijaythayyil@users.noreply.github.com>
2024-07-18 18:27:07 -0400

    Header component added and updated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import SignUp from "../src/pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />

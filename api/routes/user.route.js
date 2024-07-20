19bf7a8f2dd5c69e8e45c06b2d909cb3c0c80cbc
Vijay Thayyil <68293066+vijaythayyil@users.noreply.github.com>
2024-07-20 17:00:32 -0400

    Designs updated, new pages added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,7 +4,7 @@ import {
   updateUser,
   deleteUser,
   getUserListings,
-  //   getUser,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -14,6 +14,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
-// router.get("/:id", verifyToken, getUser);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import Profile from "./pages/Profile";
 import Home from "./pages/Home";
-import SignUp from "../src/pages/SignUp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
@@ -15,13 +17,16 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
-        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
